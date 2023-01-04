8c037d65baf85a524bf8769c1611abf82a652505
ramoncalabozo <ramoncalabozo@gmail.com>
2023-01-04 10:12:43 +0100

    teorico practico compila pero no se conecta al git

---
+++ teorico_practico/src/main/java/model/Documento.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Documento {
+
+	private Integer iddocumento;
+	private String nombre;
+	private String usuario;
+	
+	public Integer getIddocumento() {
+		return iddocumento;
+	}
+	public void setIddocumento(Integer iddocumento) {
+		this.iddocumento = iddocumento;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+}
