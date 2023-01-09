1f3f5f19df00a9375624d274f9cc964b84fcb2f4
ramoncalabozo <ramoncalabozo@gmail.com>
2023-01-09 13:26:23 +0100

    Servicio Eureka arreglado

--- configuracion_servicios/src/main/java/start/Application.java
+++ configuracion_servicios/src/main/java/start/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/* 
+/*
  * Indica que gestiona toda la configuracion de los microservicios.
  */
 
